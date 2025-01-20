cc91d1ec0409a05e370cada1021143a1a4522cca
Bruna Mara Silva <cristalbrumara@gmail.com>
2025-01-20 11:26:08 -0300

    adicionar arquivo

---
+++ geradordesenhas/Program.cs
@@ -0,0 +1,12 @@
+﻿char[] letras = {'a', 'b', 'c', 'd','e','f', 'g', 'h', 'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'y', 'z'};
+
+
+char[] numeros ={ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+
+
+char[] caracteresEspeciais ={'!' ,'@' , '#', '$', '&', '*', '()','_','%'};
+
+
+Console.WriteLine("Gerador De Senhas");
+Console.Write("sua senha deve conter quantos caracteres? (6-15)");
