49988eebf06bdc6e14ef67e972f7c8e5a9435058
Bruna Mara Silva <cristalbrumara@gmail.com>
2025-01-29 11:23:49 -0300

    adicionando iniciando gerador de senha

--- geradordesenhas/Program.cs
+++ geradordesenhas/Program.cs
@@ -1,12 +1,40 @@
-﻿char[] letras = {'a', 'b', 'c', 'd','e','f', 'g', 'h', 'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'y', 'z'};
+﻿
 
 
-char[] numeros ={ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+//solicitar a quantidade de caracteres
+Console.WriteLine("insira a quantidade de caracteres:(6-15) ");
+int quant = int.Parse(Console.ReadLine());
+
+//Perguntar ao usário se irá conter letras
+Console.WriteLine("a senha irá conter letras? (S/N) ");
+string letra = Console.ReadLine();
+
+//Perguntar ao usuário se irá conter números
+Console.WriteLine("A senha irá conter Números? (S/N) ");
+string numero = Console.ReadLine();
+
+//Perguntar ao usuário se vai ter caractere especial
+Console.WriteLine("a senha irá conter caractere especial? (S/N) ");
+string CaractEspecial = Console.ReadLine();
 
 
 
-char[] caracteresEspeciais ={'!' ,'@' , '#', '$', '&', '*', '()','_','%'};
 
 
-Console.WriteLine("Gerador De Senhas");
-Console.Write("sua senha deve conter quantos caracteres? (6-15)");
+
+
+
+// char[] letras = {'a','b', 'c', 'd','e','f', 'g', 'h', 'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'y', 'z', 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
+
+
+// char[] numeros ={ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+
+
+// char[] caracteresEspeciais ={'!' ,'@' , '#', '$', '&', '*', '()','_','%'};
+
+
+// Console.WriteLine("Gerador De Senhas");
+// Console.Write("sua senha deve conter quantos caracteres? (6-15)");
+
